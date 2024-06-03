0032d119bb0ba0f3fe9dbb52c3ddc3470c40c237
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-06-03 16:39:28 +0200

    Refactor a bit

--- scripts/displayGame.js
+++ scripts/displayGame.js
@@ -1,17 +1,26 @@
 const displayGame = (function (game, board) {
 
+  const mainEl = document.querySelector('main')
+
   const addBoardToDOM = () => {
-    const boardEl = document.createElement('div')
-    boardEl.className = 'board'
+    const boardElement = createBoardEl()
+    populateBoardElement(boardElement)
+    mainEl.appendChild(boardElement)
+  }
+
+  const populateBoardElement = (boardElement) => {
     board.gameBoard.flat().forEach((el) => {
       const gameCellEl = createBoardSquare()
       gameCellEl.textContent = el
-      boardEl.appendChild(gameCellEl)
+      boardElement.appendChild(gameCellEl)
     })
-    const mainEl = document.querySelector('main')
-    mainEl.appendChild(boardEl)
   }
 
+  const createBoardEl = () => {
+    const boardEl = document.createElement('div')
+    boardEl.className = 'board'
+    return boardEl
+  }
   const createBoardSquare = () => {
     const squareEl = document.createElement('div')
     squareEl.className = 'square'
